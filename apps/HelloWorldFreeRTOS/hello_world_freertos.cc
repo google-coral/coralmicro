0aff9b630961787ff0b8c0d9241c320b9ec5b96c
Alex Van Damme <atv@google.com>
2021-03-11 14:47:02 -0800

    Have HelloWorldFreeRTOS toggle LEDs
    GitOrigin-RevId: 4f0934bfa1db1a0258ae96fde4e80d985362c256

--- apps/HelloWorldFreeRTOS/hello_world_freertos.cc
+++ apps/HelloWorldFreeRTOS/hello_world_freertos.cc
@@ -1,12 +1,23 @@
 #include "libs/base/tasks_m7.h"
 #include "third_party/freertos_kernel/include/FreeRTOS.h"
 #include "third_party/freertos_kernel/include/task.h"
-
+#include "third_party/nxp/rt1176-sdk/devices/MIMXRT1176/drivers/fsl_gpio.h"
 #include <cstdio>
 
 extern "C" void app_main(void *param) {
     printf("Hello world FreeRTOS.\r\n");
+
+    gpio_pin_config_t user_led = {kGPIO_DigitalOutput, 0, kGPIO_NoIntmode};
+    GPIO_PinInit(GPIO13, 6, &user_led);
+
+    gpio_pin_config_t pwr_led = {kGPIO_DigitalOutput, 0, kGPIO_NoIntmode};
+    GPIO_PinInit(GPIO13, 5, &pwr_led);
+
+    bool on = true;
     while (true) {
-        taskYIELD();
+        on = !on;
+        GPIO_PinWrite(GPIO13, 5, on);
+        GPIO_PinWrite(GPIO13, 6, on);
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
