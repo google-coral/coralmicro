2321f1a25434fc0dfce043e764e07bf4f832c3c7
Alex Van Damme <atv@google.com>
2021-03-16 14:16:29 -0700

    Initial commit of BFT
    - Create BFT (board function test) to run simple tests on subsystems, to
    help ensure breakage doesn't occur.
    - Currently tested:
      - Multicore
      - TPU (TestConv1)
      - Camera test pattern
      - Audio non-zero
      - SDRam R/W
      - PMIC chip id
      - RNG
    
    GitOrigin-RevId: fa3b8f6ae0d1f8bcf0757e70efd480d119819d6a

---
+++ apps/BFT/bft.cc
@@ -0,0 +1,192 @@
+#include "apps/BFT/micro_test.h"
+#include "libs/base/ipc_m7.h"
+#include "libs/base/random.h"
+#include "libs/tasks/AudioTask/audio_task.h"
+#include "libs/tasks/CameraTask/camera_task.h"
+#include "libs/tasks/EdgeTpuTask/edgetpu_task.h"
+#include "libs/tasks/PmicTask/pmic_task.h"
+#include "libs/tensorflow/testconv1_edgetpu.h"
+#include "libs/tensorflow/testconv1_expected_output.h"
+#include "libs/tensorflow/testconv1_test_input.h"
+#include "libs/tpu/edgetpu_manager.h"
+#include "libs/tpu/edgetpu_op.h"
+#include "third_party/freertos_kernel/include/FreeRTOS.h"
+#include "third_party/freertos_kernel/include/semphr.h"
+#include "third_party/freertos_kernel/include/task.h"
+#include "third_party/tensorflow/tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "third_party/tensorflow/tensorflow/lite/micro/micro_interpreter.h"
+#include "third_party/tensorflow/tensorflow/lite/version.h"
+#include <cstdio>
+
+extern "C" void DebugLog(const char *s) {
+    printf(s);
+}
+
+namespace testing {
+
+static tflite::MicroMutableOpResolver<1> resolver;
+constexpr size_t kTensorArenaSize = 1024 * 10 * 2;
+static uint8_t tensor_arena[kTensorArenaSize] __attribute__((aligned(16)));
+
+constexpr size_t sdram_memory_size = 1024 * 1024;
+static uint8_t sdram_memory[sdram_memory_size] __attribute__((section(".sdram_bss,\"aw\",%nobits @")));
+
+constexpr size_t kAudioSamples = 512;
+static uint16_t audio_samples[kAudioSamples];
+
+TF_LITE_MICRO_TESTS_BEGIN
+
+TF_LITE_MICRO_TEST(MulticoreTest_CheckM4) {
+    TF_LITE_MICRO_EXPECT(valiant::M4IsAlive(1000 /* ms */));
+}
+
+TF_LITE_MICRO_TEST(TPUTest_CheckTestConv1) {
+    valiant::EdgeTpuTask::GetSingleton()->SetPower(true);
+    valiant::EdgeTpuManager::GetSingleton()->OpenDevice();
+
+    const tflite::Model *model = tflite::GetModel(testconv1_edgetpu_tflite);
+    TF_LITE_MICRO_EXPECT_EQ(static_cast<int>(model->version()), TFLITE_SCHEMA_VERSION);
+
+    resolver.AddCustom("edgetpu-custom-op", valiant::RegisterCustomOp());
+    tflite::MicroInterpreter interpreter(model, resolver, tensor_arena, kTensorArenaSize, micro_test::reporter);
+    TF_LITE_MICRO_EXPECT_EQ(interpreter.AllocateTensors(), kTfLiteOk);
+
+    TfLiteTensor *input = interpreter.input(0);
+    TfLiteTensor *output = interpreter.output(0);
+    TF_LITE_MICRO_EXPECT_EQ(input->bytes, testconv1_test_input_bin_len);
+    TF_LITE_MICRO_EXPECT_EQ(output->bytes, testconv1_expected_output_bin_len);
+    memcpy(tflite::GetTensorData<uint8_t>(input), testconv1_test_input_bin, testconv1_test_input_bin_len);
+
+    for (int i = 0; i < 100; ++i) {
+        memset(tflite::GetTensorData<uint8_t>(output), 0, output->bytes);
+        TF_LITE_MICRO_EXPECT_EQ(interpreter.Invoke(), kTfLiteOk);
+        TF_LITE_MICRO_EXPECT_EQ(
+                memcmp(tflite::GetTensorData<uint8_t>(output), testconv1_expected_output_bin, testconv1_expected_output_bin_len),
+                0);
+    }
+}
+
+TF_LITE_MICRO_TEST(CameraTest_CheckTestPattern) {
+    valiant::CameraTask::GetSingleton()->SetPower(true);
+    valiant::CameraTask::GetSingleton()->Enable();
+    valiant::CameraTask::GetSingleton()->SetTestPattern(
+            valiant::camera::TestPattern::WALKING_ONES);
+
+    uint8_t *buffer = nullptr;
+    int index = -1;
+    index = valiant::CameraTask::GetSingleton()->GetFrame(&buffer, true);
+
+    TF_LITE_MICRO_EXPECT_GE(index, 0);
+    TF_LITE_MICRO_EXPECT(buffer);
+
+    uint8_t expected = 0;
+    uint8_t mismatch_count = 0;
+    for (unsigned int i = 0; i < valiant::CameraTask::kWidth * valiant::CameraTask::kHeight; ++i) {
+        if (buffer[i] != expected) {
+            mismatch_count++;
+        }
+        if (expected == 0) {
+            expected = 1;
+        } else {
+            expected = expected << 1;
+        }
+    }
+    TF_LITE_MICRO_EXPECT_EQ(mismatch_count, 0);
+
+    valiant::CameraTask::GetSingleton()->ReturnFrame(index);
+    valiant::CameraTask::GetSingleton()->Disable();
+    valiant::CameraTask::GetSingleton()->SetPower(false);
+}
+
+// Check that no more kBadSamplePercentage of samples are
+// either our fill value or the default return value.
+// This helps to accomodate for the time for the microphone to start returning data
+// after clock/power application.
+// If the audio pipeline interface changes to eliminate that, reduce the percentage.
+TF_LITE_MICRO_TEST(AudioTest_CheckNonZeroSamples) {
+    constexpr float kBadSamplePercentage = 0.2;
+    constexpr uint16_t kFillValue = 0xa5a5;
+    constexpr uint16_t kDefaultValue = 0x9000;
+    SemaphoreHandle_t sema = xSemaphoreCreateBinary();
+    memset(audio_samples, kFillValue, ARRAY_SIZE(audio_samples));
+    valiant::AudioTask::GetSingleton()->SetCallback([](void *param) {
+        static int buffer_count = 0;
+        if (buffer_count == 4) {
+            BaseType_t reschedule = pdFALSE;
+            SemaphoreHandle_t sema = reinterpret_cast<SemaphoreHandle_t>(param);
+            xSemaphoreGiveFromISR(sema, &reschedule);
+            portYIELD_FROM_ISR(reschedule);
+            return reinterpret_cast<uint32_t*>(0);
+        } else {
+            buffer_count++;
+            return reinterpret_cast<uint32_t*>(audio_samples);
+        }
+    }, sema);
+    valiant::AudioTask::GetSingleton()->SetPower(true);
+    valiant::AudioTask::GetSingleton()->SetBuffer(reinterpret_cast<uint32_t*>(audio_samples), sizeof(audio_samples));
+    valiant::AudioTask::GetSingleton()->Enable();
+    xSemaphoreTake(sema, pdMS_TO_TICKS(1000));
+
+    int non_matching_count = 0;
+    for (size_t i = 0; i < ARRAY_SIZE(audio_samples); ++i) {
+        if (audio_samples[i] == kFillValue || audio_samples[i] == kDefaultValue) {
+            non_matching_count++;
+        }
+    }
+    TF_LITE_MICRO_EXPECT_LT(non_matching_count, static_cast<int>(kAudioSamples * kBadSamplePercentage));
+    if (non_matching_count >= static_cast<int>(kAudioSamples * kBadSamplePercentage)) {
+        printf("non_matching_count: %d\r\n", non_matching_count);
+    }
+
+    valiant::AudioTask::GetSingleton()->Disable();
+    valiant::AudioTask::GetSingleton()->SetPower(false);
+    vSemaphoreDelete(sema);
+}
+
+TF_LITE_MICRO_TEST(SDRAMTest_CheckReadWrite) {
+    for (size_t i = 0; i < sdram_memory_size; ++i) {
+        sdram_memory[i] = (i % 256);
+    }
+    for (size_t i = 0; i < sdram_memory_size; ++i) {
+        TF_LITE_MICRO_EXPECT_EQ(sdram_memory[i], (i % 256));
+    }
+}
+
+TF_LITE_MICRO_TEST(NANDTest_CheckSomething) {
+    // Maybe read back the beginning of our binary and compare it?
+    micro_test::reporter->Report("\t - NANDTest_CheckSomething unimplemented");
+}
+
+TF_LITE_MICRO_TEST(PmicTest_CheckChipId) {
+    TF_LITE_MICRO_EXPECT_EQ(valiant::PmicTask::GetSingleton()->GetChipId(), 0x62);
+}
+
+TF_LITE_MICRO_TEST(RandomTest_CheckRandom) {
+    constexpr int kRandomValues = 4;
+    constexpr uint8_t kConstByte = 0xa5;
+    constexpr uint32_t kConstInt = 0xa5a5a5a5;
+    uint32_t data[kRandomValues];
+    memset(data, kConstByte, sizeof(data));
+
+    bool success = valiant::Random::GetSingleton()->GetRandomNumber(data, sizeof(data));
+    TF_LITE_MICRO_EXPECT(success);
+
+    for (size_t i = 0; i < ARRAY_SIZE(data); ++i) {
+        TF_LITE_MICRO_EXPECT_NE(data[i], kConstInt);
+    }
+}
+
+if (micro_test::tests_failed == 0) {
+    printf("Blink LEDs success\r\n");
+} else {
+    printf("Blink LEDs failure\r\n");
+}
+
+TF_LITE_MICRO_TESTS_END
+
+}  // namespace testing
+
+extern "C" void app_main(void *param) {
+    testing::main(0, NULL);
+    vTaskSuspend(NULL);
+}
---
+++ apps/BFT/bft_m4.cc
@@ -0,0 +1,6 @@
+#include "third_party/freertos_kernel/include/FreeRTOS.h"
+#include "third_party/freertos_kernel/include/task.h"
+
+extern "C" void app_main(void *param) {
+    vTaskSuspend(NULL);
+}
