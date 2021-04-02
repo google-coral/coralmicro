6465da731a25c5155d63a356fa656835a5139861
Alex Van Damme <atv@google.com>
2021-04-02 14:07:05 -0700

    Updates to SDRAM configuration
    GitOrigin-RevId: ee4b9e406d796a1dc34bb5a3ba038c5d037c0b31

--- libs/nxp/rt1176-sdk/board_hardware.c
+++ libs/nxp/rt1176-sdk/board_hardware.c
@@ -52,21 +52,21 @@ bool BOARD_InitSEMC(void) {
     /* Configure SDRAM. */
     sdramconfig.csxPinMux           = kSEMC_MUXCSX0;
     sdramconfig.address             = 0x80000000;
-    sdramconfig.memsize_kbytes      = 32 * 1024; /* 32MB = 32*1024*1KBytes*/
+    sdramconfig.memsize_kbytes      = 64 * 1024; /* 64MB = 64*1024*1KBytes*/
     sdramconfig.portSize            = kSEMC_PortSize16Bit;
     sdramconfig.burstLen            = kSEMC_Sdram_BurstLen8;
-    sdramconfig.columnAddrBitNum    = kSEMC_SdramColunm_9bit;
+    sdramconfig.columnAddrBitNum    = kSEMC_SdramColunm_10bit;
     sdramconfig.casLatency          = kSEMC_LatencyThree;
-    sdramconfig.tPrecharge2Act_Ns   = 15; /* tRP 15ns */
-    sdramconfig.tAct2ReadWrite_Ns   = 15; /* tRCD 15ns */
-    sdramconfig.tRefreshRecovery_Ns = 70; /* Use the maximum of the (Trfc , Txsr). */
-    sdramconfig.tWriteRecovery_Ns   = 2;  /* tWR 2ns */
+    sdramconfig.tPrecharge2Act_Ns   = 18; /* tRP */
+    sdramconfig.tAct2ReadWrite_Ns   = 18; /* tRCD */
+    sdramconfig.tRefreshRecovery_Ns = 120; /* Use the maximum of the (Trfc , Txsr). */
+    sdramconfig.tWriteRecovery_Ns   = 15;  /* tWR */
     sdramconfig.tCkeOff_Ns =
         42; /* The minimum cycle of SDRAM CLK off state. CKE is off in self refresh at a minimum period tRAS.*/
-    sdramconfig.tAct2Prechage_Ns       = 40; /* tRAS 40ns */
+    sdramconfig.tAct2Prechage_Ns       = 42; /* tRAS */
     sdramconfig.tSelfRefRecovery_Ns    = 70;
     sdramconfig.tRefresh2Refresh_Ns    = 60;
-    sdramconfig.tAct2Act_Ns            = 2; /* tRC/tRDD 2ns */
+    sdramconfig.tAct2Act_Ns            = 30; /* tRC/tRDD */
     sdramconfig.tPrescalePeriod_Ns     = 160 * (1000000000 / clockFrq);
     sdramconfig.refreshPeriod_nsPerRow = 64 * 1000000 / 8192; /* 64ms/8192 */
     sdramconfig.refreshUrgThreshold    = sdramconfig.refreshPeriod_nsPerRow;
