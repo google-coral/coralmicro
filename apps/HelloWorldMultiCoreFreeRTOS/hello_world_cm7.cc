798cf63217875a3d771724acfb7ba05bff9056ca
Alex Van Damme <atv@google.com>
2021-06-04 13:46:11 -0700

    Add StartM4 to HelloWorldMultiCoreFreeRTOS
    - This one got missed when M4 was changed to not start automatically.
    
    Change-Id: Ib44a7fa1c39a2cd776daf94e3641b5c173b77ea2
    GitOrigin-RevId: b2adddfaff9c8e2ea04bbe534a56bd4e5782728d

--- apps/HelloWorldMultiCoreFreeRTOS/hello_world_cm7.cc
+++ apps/HelloWorldMultiCoreFreeRTOS/hello_world_cm7.cc
@@ -1,9 +1,11 @@
+#include "libs/base/ipc_m7.h"
 #include "third_party/freertos_kernel/include/FreeRTOS.h"
 #include "third_party/freertos_kernel/include/task.h"
 #include <cstdio>
 #include <cstring>
 
 extern "C" void app_main(void *param) {
+    static_cast<valiant::IPCM7*>(valiant::IPC::GetSingleton())->StartM4();
     while (true) {
         printf("[M7] Hello.\r\n");
         vTaskDelay(pdMS_TO_TICKS(500));
