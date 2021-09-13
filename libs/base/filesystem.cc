7449a303b86eb5d972cced1194191fdc16c9237b
Alex Van Damme <atv@google.com>
2021-09-13 16:45:00 -0700

    Double filesystem size to 16MB
    - This allows fitting of larger models than the previous 8MB filesystem.
    
    Change-Id: I4f101c8454f25fe36f09c090059be1c0c652fcfe
    GitOrigin-RevId: a26a96fd05670718059d34d1eba09250a1493d57

--- libs/base/filesystem.cc
+++ libs/base/filesystem.cc
@@ -100,7 +100,7 @@ bool Init() {
     lfs_config_.read_size = 2048;
     lfs_config_.prog_size = 2048;
     lfs_config_.block_size = 131072;
-    lfs_config_.block_count = 64;
+    lfs_config_.block_count = 128;
     lfs_config_.block_cycles = 250;
     lfs_config_.cache_size = 2048;
     lfs_config_.lookahead_size = 2048;
