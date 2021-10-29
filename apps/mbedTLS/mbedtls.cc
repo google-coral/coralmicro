3618b8eaf7fde4ee080b5958d59aed693275779f
Alex Van Damme <atv@google.com>
2021-10-29 10:54:04 -0700

    Flush out mbedTLS library, add selftest app
    - Add an mbedTLS app, that runs selftests for a whole set of suites.
    - Import and customize ksdk_mbedtls_config.h from the SDK.
    - Add implementations of some support functions that require
    customization for our platform.
    - At the moment, this isn't hardware accelerated. It's possible to
    enable with a define, but correct function requires disabling the
    DCache -- which kills performance. Revisit this when we have devices
    with working A71CH, and can use the SSS backend.
    
    Change-Id: I3d9a5db48903037e8e60a99da378c3796f2630d1
    GitOrigin-RevId: fbbe4c21386048e96179cdec2a7454539b2289d2

---
+++ apps/mbedTLS/mbedtls.cc
@@ -0,0 +1,67 @@
+#include "third_party/freertos_kernel/include/FreeRTOS.h"
+#include "third_party/freertos_kernel/include/task.h"
+
+#include MBEDTLS_CONFIG_FILE
+
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/base64.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/ccm.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/chacha20.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/chachapoly.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/ctr_drbg.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/des.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/dhm.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/ecp.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/gcm.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/hmac_drbg.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/md5.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/pkcs5.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/poly1305.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/ripemd160.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/rsa.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/sha1.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/sha256.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/sha512.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/x509.h"
+
+#include <cstdio>
+
+// From third_party/nxp/rt1176-sdk/boards/evkmimxrt1170/mbedtls_examples/mbedtls_selftest/cm7/selftest.c
+typedef struct
+{
+    const char *name;
+    int ( *function )( int /* verbose */ );
+} selftest_t;
+
+const selftest_t selftests[] =
+{
+    {"aes", mbedtls_aes_self_test},
+    {"base64", mbedtls_base64_self_test},
+    {"ccm", mbedtls_ccm_self_test},
+    {"chacha20", mbedtls_chacha20_self_test},
+    {"chacha20-poly1305", mbedtls_chachapoly_self_test},
+    {"ctr_drbg", mbedtls_ctr_drbg_self_test},
+    {"des", mbedtls_des_self_test},
+    {"dhm", mbedtls_dhm_self_test},
+    {"ecp", mbedtls_ecp_self_test},
+    {"gcm", mbedtls_gcm_self_test},
+    {"hmac_drbg", mbedtls_hmac_drbg_self_test},
+    {"md5", mbedtls_md5_self_test},
+    {"mpi", mbedtls_mpi_self_test},
+    {"pkcs5", mbedtls_pkcs5_self_test},
+    {"poly1305", mbedtls_poly1305_self_test},
+    {"ripemd160", mbedtls_ripemd160_self_test},
+    {"rsa", mbedtls_rsa_self_test},
+    {"sha1", mbedtls_sha1_self_test},
+    {"sha256", mbedtls_sha256_self_test},
+    {"sha512", mbedtls_sha512_self_test},
+    {"x509", mbedtls_x509_self_test},
+    {NULL, NULL}
+};
+
+extern "C" void app_main(void *param) {
+    for (const selftest_t* test = selftests; test->name != NULL; test++) {
+        test->function(1);
+    }
+
+    vTaskSuspend(NULL);
+}
---
+++ libs/nxp/rt1176-sdk/mbedtls.cc
@@ -0,0 +1,186 @@
+#include <cstdarg>
+#include <cstdio>
+#include <cstring>
+#include <memory>
+
+#include "libs/base/filesystem.h"
+#include "third_party/nxp/rt1176-sdk/middleware/lwip/src/include/lwip/sockets.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/net_sockets.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/pk.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/platform.h"
+#include "third_party/nxp/rt1176-sdk/middleware/mbedtls/include/mbedtls/ssl.h"
+
+extern "C" int valiant_mbedtls_printf(const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+  int formatted_size = vsnprintf(NULL, 0, format, args) + 1;
+  auto formatted_string = std::make_unique<char[]>(formatted_size);
+  vsnprintf(formatted_string.get(), formatted_size, format, args);
+  for (size_t i = 0; i < strlen(formatted_string.get()); ++i) {
+    char c = formatted_string[i];
+    if (c == '\n') {
+      putchar('\r');
+    }
+    putchar(formatted_string[i]);
+  }
+  va_end(args);
+  return formatted_size;
+}
+
+// The below are implementations based on
+// third_party/nxp/rt1176-sdk/middleware/mbedtls/library/net_sockets.c In a
+// "normal" mbedtls build, these require something Unixy, or Windows. These
+// modify the stock implementations to work with our network stack (lwip).
+static int net_would_block(const mbedtls_net_context *ctx) {
+  int err = errno;
+
+  if ((fcntl(ctx->fd, F_GETFL, 0) & O_NONBLOCK) != O_NONBLOCK) {
+    errno = err;
+    return 0;
+  }
+
+  switch (errno = err) {
+    case EAGAIN:
+      return 1;
+  }
+  return 0;
+}
+
+extern "C" int mbedtls_net_send(void *ctx, unsigned char const *buf,
+                                size_t len) {
+  int ret;
+  int fd = (reinterpret_cast<mbedtls_net_context *>(ctx))->fd;
+
+  if (fd < 0) {
+    return MBEDTLS_ERR_NET_INVALID_CONTEXT;
+  }
+
+  ret = write(fd, buf, len);
+
+  if (ret < 0) {
+    if (net_would_block(reinterpret_cast<mbedtls_net_context *>(ctx)) != 0) {
+      return MBEDTLS_ERR_SSL_WANT_WRITE;
+    }
+
+    if (errno == EPIPE || errno == ECONNRESET) {
+      return MBEDTLS_ERR_NET_CONN_RESET;
+    }
+
+    if (errno == EINTR) {
+      return MBEDTLS_ERR_SSL_WANT_WRITE;
+    }
+
+    return MBEDTLS_ERR_NET_SEND_FAILED;
+  }
+
+  return ret;
+}
+
+extern "C" int mbedtls_net_recv(void *ctx, unsigned char *buf, size_t len) {
+  int ret;
+  int fd = (reinterpret_cast<mbedtls_net_context *>(ctx))->fd;
+
+  if (fd < 0) {
+    return MBEDTLS_ERR_NET_INVALID_CONTEXT;
+  }
+
+  ret = (int)read(fd, buf, len);
+
+  if (ret < 0) {
+    if (net_would_block(reinterpret_cast<mbedtls_net_context *>(ctx)) != 0) {
+      return MBEDTLS_ERR_SSL_WANT_READ;
+    }
+
+    if (errno == EPIPE || errno == ECONNRESET) {
+      return MBEDTLS_ERR_NET_CONN_RESET;
+    }
+
+    if (errno == EINTR) {
+      return MBEDTLS_ERR_SSL_WANT_READ;
+    }
+
+    return MBEDTLS_ERR_NET_RECV_FAILED;
+  }
+
+  return ret;
+}
+
+// The below are based on implementations in
+// third_party/nxp/rt1176-sdk/middleware/mbedtls/library/pkparse.c In a normal
+// curl build, these require filesystem support. While we have a filesystem,
+// it's not accessible by normal things like fopen. Adapt the original
+// implementation to use our filesystem API.
+extern "C" int mbedtls_pk_load_file(const char *path, unsigned char **buf,
+                                    size_t *n) {
+  MBEDTLS_INTERNAL_VALIDATE_RET(path != NULL, MBEDTLS_ERR_PK_BAD_INPUT_DATA);
+  MBEDTLS_INTERNAL_VALIDATE_RET(buf != NULL, MBEDTLS_ERR_PK_BAD_INPUT_DATA);
+  MBEDTLS_INTERNAL_VALIDATE_RET(n != NULL, MBEDTLS_ERR_PK_BAD_INPUT_DATA);
+
+  lfs_file_t handle;
+  if (!valiant::filesystem::Open(&handle, path)) {
+    return MBEDTLS_ERR_PK_FILE_IO_ERROR;
+  }
+
+  *n = valiant::filesystem::Size(&handle);
+
+  if (*n + 1 == 0 || (*buf = reinterpret_cast<unsigned char *>(
+                          mbedtls_calloc(1, *n + 1))) == NULL) {
+    valiant::filesystem::Close(&handle);
+    return MBEDTLS_ERR_PK_ALLOC_FAILED;
+  }
+
+  if (static_cast<size_t>(valiant::filesystem::Read(&handle, *buf, *n)) != *n) {
+    valiant::filesystem::Close(&handle);
+
+    mbedtls_platform_zeroize(*buf, *n);
+    mbedtls_free(*buf);
+
+    return MBEDTLS_ERR_PK_FILE_IO_ERROR;
+  }
+
+  valiant::filesystem::Close(&handle);
+
+  (*buf)[*n] = '\0';
+
+  if (strstr(reinterpret_cast<const char *>(*buf), "-----BEGIN ") != NULL) ++*n;
+
+  return (0);
+}
+
+extern "C" int mbedtls_pk_parse_keyfile(mbedtls_pk_context *ctx,
+                                        const char *path, const char *pwd) {
+  return MBEDTLS_ERR_PK_FILE_IO_ERROR;
+}
+
+// The below are based on implementations in
+// third_party/nxp/rt1176-sdk/middleware/mbedtls/library/x509_crt.c In a normal
+// curl build, these require filesystem support. While we have a filesystem,
+// it's not accessible by normal things like fopen. Adapt the original
+// implementation to use our filesystem API.
+extern "C" int mbedtls_x509_crt_parse_file(mbedtls_x509_crt *chain,
+                                           const char *path) {
+  int ret;
+  size_t n;
+  unsigned char *buf;
+
+  if ((ret = mbedtls_pk_load_file(path, &buf, &n)) != 0) {
+    return ret;
+  }
+
+  ret = mbedtls_x509_crt_parse(chain, buf, n);
+
+  mbedtls_platform_zeroize(buf, n);
+  mbedtls_free(buf);
+
+  return ret;
+}
+
+extern "C" int mbedtls_x509_crl_parse_file(mbedtls_x509_crl *chain,
+                                           const char *path) {
+  return MBEDTLS_ERR_X509_FILE_IO_ERROR;
+}
+
+extern "C" int mbedtls_x509_crt_parse_path(mbedtls_x509_crt *chain,
+                                           const char *path) {
+  return MBEDTLS_ERR_X509_FILE_IO_ERROR;
+}
