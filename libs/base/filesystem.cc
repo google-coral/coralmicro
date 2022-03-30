7fbe84d51975a510fa3e555fdac552a7d3daee5b
Dmitry Kovalev <dkovalev@google.com>
2022-03-30 15:25:17 -0700

    Add locking via littlefs hooks
    ReadFile/WriteFile were reimplemented using littlefs API
    directly.
    
    Change-Id: Ie4dff6b8b70176fc6085a5d81adaee873f2d69a5
    GitOrigin-RevId: 25f87aea39522d163a16cdd2a76458de3ca7e288

--- libs/base/filesystem.cc
+++ libs/base/filesystem.cc
@@ -1,10 +1,11 @@
-#include "libs/base/mutex.h"
 #include "libs/base/filesystem.h"
 #include "third_party/freertos_kernel/include/FreeRTOS.h"
 #include "third_party/freertos_kernel/include/semphr.h"
 #include "third_party/nxp/rt1176-sdk/components/flash/nand/fsl_nand_flash.h"
 
+#include <algorithm>
 #include <cstdio>
+#include <cstring>
 #include <memory>
 
 extern "C" nand_handle_t* BOARD_GetNANDHandle(void);
@@ -12,19 +13,18 @@ extern "C" nand_handle_t* BOARD_GetNANDHandle(void);
 namespace valiant {
 namespace filesystem {
 namespace {
-
-struct AutoClose {
-  lfs_file_t* handle;
-  ~AutoClose() { Close(handle); }
-};
-
-lfs_t lfs_handle_;
-lfs_config lfs_config_;
-SemaphoreHandle_t lfs_semaphore_;
+lfs_t g_lfs;
+lfs_config g_lfs_config;
+SemaphoreHandle_t g_lfs_mutex;
 
 constexpr int kPagesPerBlock = 64;
 constexpr int kFilesystemBaseBlock = 12;
 constexpr lfs_size_t kPageSize = 2048;
+
+struct AutoClose {
+  lfs_file_t* file;
+  ~AutoClose() { lfs_file_close(&g_lfs, file); }
+};
 
 int LfsRead(const struct lfs_config *c, lfs_block_t block, lfs_off_t off,
             void *buffer, lfs_size_t size) {
@@ -81,48 +81,61 @@ int LfsSync(const struct lfs_config *c) {
     return LFS_ERR_OK;
 }
 
+int LfsLock(const struct lfs_config *c) {
+    if (xSemaphoreTake(g_lfs_mutex, portMAX_DELAY) != pdTRUE)
+        return -1;
+    return 0;
+}
+
+int LfsUnlock(const struct lfs_config *c) {
+    if (xSemaphoreGive(g_lfs_mutex) != pdTRUE)
+        return -1;
+    return 0;
+}
 }  // namespace
 
 bool Init(bool force_format) {
-    memset(&lfs_config_, 0, sizeof(lfs_config_));
-    lfs_config_.read = LfsRead;
-    lfs_config_.prog = LfsProg;
-    lfs_config_.erase = LfsErase;
-    lfs_config_.sync = LfsSync;
-    lfs_config_.read_size = kPageSize;
-    lfs_config_.prog_size = kPageSize;
-    lfs_config_.block_size = 131072;
-    lfs_config_.block_count = 512;
-    lfs_config_.block_cycles = 250;
-    lfs_config_.cache_size = kPageSize;
-    lfs_config_.lookahead_size = kPageSize;
+    if (g_lfs_mutex) vSemaphoreDelete(g_lfs_mutex);
+
+    g_lfs_mutex = xSemaphoreCreateMutex();
+    if (!g_lfs_mutex) return false;
+
+    std::memset(&g_lfs_config, 0, sizeof(g_lfs_config));
+    g_lfs_config.read = LfsRead;
+    g_lfs_config.prog = LfsProg;
+    g_lfs_config.erase = LfsErase;
+    g_lfs_config.sync = LfsSync;
+    g_lfs_config.lock = LfsLock;
+    g_lfs_config.unlock = LfsUnlock;
+    g_lfs_config.read_size = kPageSize;
+    g_lfs_config.prog_size = kPageSize;
+    g_lfs_config.block_size = 131072;
+    g_lfs_config.block_count = 512;
+    g_lfs_config.block_cycles = 250;
+    g_lfs_config.cache_size = kPageSize;
+    g_lfs_config.lookahead_size = kPageSize;
 
     if (force_format) {
-        int ret = lfs_format(&lfs_handle_, &lfs_config_);
+        int ret = lfs_format(&g_lfs, &g_lfs_config);
         if (ret < 0) {
             return false;
         }
     }
 
-    int ret = lfs_mount(&lfs_handle_, &lfs_config_);
+    int ret = lfs_mount(&g_lfs, &g_lfs_config);
     if (ret < 0) {
         // No filesystem detected, go ahead and format.
-        ret = lfs_format(&lfs_handle_, &lfs_config_);
+        ret = lfs_format(&g_lfs, &g_lfs_config);
         if (ret < 0) {
             return false;
         }
 
         // Filesystem should exist now, mount up.
         // If this fails, return an error.
-        ret = lfs_mount(&lfs_handle_, &lfs_config_);
+        ret = lfs_mount(&g_lfs, &g_lfs_config);
         if (ret < 0) {
             return false;
         }
-    }
-
-    lfs_semaphore_ = xSemaphoreCreateMutex();
-    if (!lfs_semaphore_) {
-        return false;
     }
     return true;
 }
@@ -132,16 +145,12 @@ bool Open(lfs_file_t* handle, const char *path) {
 }
 
 bool Open(lfs_file_t* handle, const char *path, bool writable, bool append) {
-    int ret;
-    MutexLock lock(lfs_semaphore_);
-    ret = lfs_file_open(&lfs_handle_, handle, path, writable ? ((append ? LFS_O_APPEND : LFS_O_TRUNC) | LFS_O_CREAT | LFS_O_RDWR) : LFS_O_RDONLY);
-
+    int ret = lfs_file_open(&g_lfs, handle, path, writable ? ((append ? LFS_O_APPEND : LFS_O_TRUNC) | LFS_O_CREAT | LFS_O_RDWR) : LFS_O_RDONLY);
     return (ret == LFS_ERR_OK);
 }
 
 int Write(lfs_file_t *handle, const void *buffer, size_t size) {
-    MutexLock lock(lfs_semaphore_);
-    return lfs_file_write(&lfs_handle_, handle, buffer, size);
+    return lfs_file_write(&g_lfs, handle, buffer, size);
 }
 
 std::unique_ptr<char[]> Dirname(const char *path) {
@@ -157,34 +166,33 @@ std::unique_ptr<char[]> Dirname(const char *path) {
     }
 
     std::unique_ptr<char[]> dir(new char[last_separator + 1]);
-    memset(dir.get(), 0, last_separator + 1);
-    memcpy(dir.get(), path, last_separator);
+    std::memset(dir.get(), 0, last_separator + 1);
+    std::memcpy(dir.get(), path, last_separator);
     return dir;
 }
 
 bool MakeDirs(const char *path) {
     int ret;
     size_t path_len = strlen(path);
-    if (path_len > lfs_handle_.name_max) {
+    if (path_len > g_lfs.name_max) {
         return false;
     }
 
-    MutexLock lock(lfs_semaphore_);
     std::unique_ptr<char[]> path_copy(new char[path_len + 1]);
-    memset(path_copy.get(), 0, path_len + 1);
-    memcpy(path_copy.get(), path, path_len);
+    std::memset(path_copy.get(), 0, path_len + 1);
+    std::memcpy(path_copy.get(), path, path_len);
 
     for (size_t i = 1; i < path_len; ++i) {
         if (path_copy[i] == '/') {
             path_copy[i] = 0;
-            ret = lfs_mkdir(&lfs_handle_, path_copy.get());
+            ret = lfs_mkdir(&g_lfs, path_copy.get());
             if (ret < 0 && ret != LFS_ERR_EXIST) {
                 return false;
             }
             path_copy[i] = '/';
         }
     }
-    ret = lfs_mkdir(&lfs_handle_, path_copy.get());
+    ret = lfs_mkdir(&g_lfs, path_copy.get());
     if (ret < 0 && ret != LFS_ERR_EXIST) {
         return false;
     }
@@ -193,79 +201,45 @@ bool MakeDirs(const char *path) {
 }
 
 bool OpenDir(lfs_dir_t *dir, const char *path) {
-    int ret;
-    MutexLock lock(lfs_semaphore_);
-    ret = lfs_dir_open(&lfs_handle_, dir, path);
-
-    if (ret < 0) {
-        return false;
-    }
-
+    int ret = lfs_dir_open(&g_lfs, dir, path);
+    if (ret < 0) return false;
     return true;
 }
 
 bool ReadDir(lfs_dir_t *dir, lfs_info *info)  {
-    int ret;
-    MutexLock lock(lfs_semaphore_);
-    ret = lfs_dir_read(&lfs_handle_, dir, info);
-
-    if (ret <= 0) {
-        return false;
-    }
-
+    int ret = lfs_dir_read(&g_lfs, dir, info);
+    if (ret <= 0) return false;
     return true;
 }
 
 
 bool CloseDir(lfs_dir_t *dir) {
-    int ret;
-    MutexLock lock(lfs_semaphore_);
-    ret = lfs_dir_close(&lfs_handle_, dir);
-
-    if (ret < 0) {
-        return false;
-    }
-
+    int ret = lfs_dir_close(&g_lfs, dir);
+    if (ret < 0) return false;
     return true;
 }
 
 bool RewindDir(lfs_dir_t *dir) {
-    int ret;
-    MutexLock lock(lfs_semaphore_);
-    ret = lfs_dir_rewind(&lfs_handle_, dir);
-
-    if (ret < 0) {
-        return false;
-    }
-
+    int ret = lfs_dir_rewind(&g_lfs, dir);
+    if (ret < 0) return false;
     return true;
 }
 
 int Read(lfs_file_t* handle, void *buffer, size_t size) {
-    int ret;
-    MutexLock lock(lfs_semaphore_);
-    ret = lfs_file_read(&lfs_handle_, handle, buffer, size);
-
-    return ret;
+    return lfs_file_read(&g_lfs, handle, buffer, size);
 }
 
 bool Seek(lfs_file_t* handle, size_t off, int whence) {
-    int ret;
-    MutexLock lock(lfs_semaphore_);
-    ret = lfs_file_seek(&lfs_handle_, handle, off, whence);
-
+    int ret = lfs_file_seek(&g_lfs, handle, off, whence);
     return (ret >= 0) ? true : false;
 }
 
 size_t Position(lfs_file_t* handle) {
-    return lfs_file_seek(&lfs_handle_, handle, 0, LFS_SEEK_CUR);
+    return lfs_file_seek(&g_lfs, handle, 0, LFS_SEEK_CUR);
 }
 
 bool Close(lfs_file_t* handle) {
-    int ret;
-    MutexLock lock(lfs_semaphore_);
-    ret = lfs_file_close(&lfs_handle_, handle);
-
+    int ret = lfs_file_close(&g_lfs, handle);
     return (ret == LFS_ERR_OK) ? true : false;
 }
 
@@ -281,85 +255,85 @@ ssize_t Size(const char *path) {
 }
 
 lfs_soff_t Size(lfs_file_t* handle) {
-    MutexLock lock(lfs_semaphore_);
-    return lfs_file_size(&lfs_handle_, handle);
+    return lfs_file_size(&g_lfs, handle);
 }
 
 bool Remove(const char *path) {
-    MutexLock lock(lfs_semaphore_);
-    int ret = lfs_remove(&lfs_handle_, path);
-    if (ret < 0) {
-        return false;
-    }
+    int ret = lfs_remove(&g_lfs, path);
+    if (ret < 0) return false;
     return true;
 }
 
 bool Stat(const char *path, struct lfs_info *info) {
-    MutexLock lock(lfs_semaphore_);
-    int ret = lfs_stat(&lfs_handle_, path, info);
-    if (ret < 0) {
-        return false;
-    }
+    int ret = lfs_stat(&g_lfs, path, info);
+    if (ret < 0) return false;
     return true;
 }
 
 bool DirOpen(lfs_dir_t *dir, const char *path) {
-    MutexLock lock(lfs_semaphore_);
-    int ret = lfs_dir_open(&lfs_handle_, dir, path);
-    if (ret < 0) {
-        return false;
-    }
+    int ret = lfs_dir_open(&g_lfs, dir, path);
+    if (ret < 0) return false;
     return true;
 }
 
 int DirRead(lfs_dir_t *dir, struct lfs_info *info) {
-    MutexLock lock(lfs_semaphore_);
-    return lfs_dir_read(&lfs_handle_, dir, info);
+    return lfs_dir_read(&g_lfs, dir, info);
 }
 
 bool DirClose(lfs_dir_t *dir) {
-    MutexLock lock(lfs_semaphore_);
-    int ret = lfs_dir_close(&lfs_handle_, dir);
-    if (ret < 0) {
-        return false;
-    }
+    int ret = lfs_dir_close(&g_lfs, dir);
+    if (ret < 0) return false;
     return true;
 }
 
 bool ReadFile(const char* path, std::vector<uint8_t>* buf) {
-    lfs_file_t handle;
-    if (!Open(&handle, path)) return false;
-    AutoClose close{&handle};
-    buf->resize(Size(&handle));
-    if (Read(&handle, buf->data(), buf->size()) < 0) return false;
-    return true;
+    lfs_file_t file;
+    if (lfs_file_open(&g_lfs, &file, path, LFS_O_RDONLY) < 0) return false;
+    AutoClose close{&file};
+
+    auto file_size = lfs_file_size(&g_lfs, &file);
+    if (file_size < 0) return false;
+    buf->resize(file_size);
+
+    auto n = lfs_file_read(&g_lfs, &file, buf->data(), buf->size());
+    return n >= 0 && static_cast<size_t>(n) == buf->size();
 }
 
 bool ReadFile(const char* path, std::string* str) {
-    lfs_file_t handle;
-    if (!Open(&handle, path)) return false;
-    AutoClose close{&handle};
-    str->resize(Size(&handle));
-    if (Read(&handle, str->data(), str->size()) < 0) return false;
-    return true;
+    lfs_file_t file;
+    if (lfs_file_open(&g_lfs, &file, path, LFS_O_RDONLY) < 0) return false;
+    AutoClose close{&file};
+
+    auto file_size = lfs_file_size(&g_lfs, &file);
+    if (file_size < 0) return false;
+    str->resize(file_size);
+
+    auto n = lfs_file_read(&g_lfs, &file, str->data(), str->size());
+    return n >= 0 && static_cast<size_t>(n) == str->size();
 }
 
 size_t ReadFile(const char* path, uint8_t* buf, size_t size) {
-    lfs_file_t handle;
-    if (!Open(&handle, path)) return 0;
-    AutoClose close{&handle};
-    auto read_size = std::min(size, static_cast<size_t>(Size(&handle)));
-    if (Read(&handle, buf, read_size) < 0) return 0;
-    return read_size;
+    lfs_file_t file;
+    if (lfs_file_open(&g_lfs, &file, path, LFS_O_RDONLY) < 0) return false;
+    AutoClose close{&file};
+
+    auto file_size = lfs_file_size(&g_lfs, &file);
+    if (file_size < 0) return false;
+    size = std::min(size, static_cast<size_t>(file_size));
+
+    auto n = lfs_file_read(&g_lfs, &file, buf, size);
+    return n >= 0 && static_cast<size_t>(n) == size;
 }
 
 bool WriteFile(const char* path, const uint8_t* buf, size_t size) {
-    lfs_file_t handle;
-    if (!Open(&handle, path, /*writable=*/true)) return false;
-    AutoClose close{&handle};
-    if (Write(&handle, buf, size) != static_cast<int>(size)) return false;
-    return true;
-}
+    lfs_file_t file;
+    if (lfs_file_open(&g_lfs, &file, path,
+                      LFS_O_WRONLY | LFS_O_TRUNC | LFS_O_CREAT) < 0)
+        return false;
+    AutoClose close{&file};
 
+    auto n = lfs_file_write(&g_lfs, &file, buf, size);
+    return n >= 0 && static_cast<size_t>(n) == size;
+}
 }  // namespace filesystem
 }  // namespace valiant
