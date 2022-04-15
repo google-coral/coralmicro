87022d38eb3bec9785ef1da2a902f995670a32cf
Nam Vu <vunam@google.com>
2022-04-15 14:15:42 -0500

    Added Camera image server app for manual testing (b/228077485).
    Build and flash:
    - USB:
        ninja -C build -j4 && scripts/flashtool.py --build_dir build --app ImageServer --subapp ImageServerUSB --usb_address=10.10.10.1 --ram
    - Ethernet:
        ninja -C build -j4 && scripts/flashtool.py --build_dir build --app ImageServer --subapp ImageServerEthernet --usb_address=10.10.10.1 --ram
    
    Client Run:
    - USB:
        python3 apps/ImageServerPOE/image_server_test.py --usb_ip 10.10.10.1 --image_width 300 --image_height 300
    - Ethernet with USB:
        python3 apps/ImageServerPOE/image_server_test.py --usb_ip 10.10.10.1 --image_width 300 --image_height 300 --ethernet
    - Pure POE (no USB attached):
        python3 apps/ImageServerPOE/image_server_test.py --usb_ip 10.10.10.1 --image_width 300 --image_height 300 --ethernet --ethernet_ip=192.168.1.117
    
    Change-Id: Ia4923d2669f7e5221134afd9853d50ad66b0c287
    GitOrigin-RevId: 2b16dbecbd96d3f1140481325e74eac32d7bf6cf

---
+++ apps/ImageServer/image_server.cc
@@ -0,0 +1,86 @@
+#include <cstdio>
+
+#include "libs/RPCServer/rpc_server_io_http.h"
+#include "libs/tasks/CameraTask/camera_task.h"
+#include "libs/testlib/test_lib.h"
+#include "third_party/freertos_kernel/include/FreeRTOS.h"
+#include "third_party/freertos_kernel/include/task.h"
+#include "third_party/mjson/src/mjson.h"
+
+#if defined(IMAGE_SERVER_ETHERNET)
+#include "libs/base/ethernet.h"
+#include "third_party/nxp/rt1176-sdk/middleware/lwip/src/include/lwip/prot/dhcp.h"
+#endif  // defined(IMAGE_SERVER_ETHERNET)
+
+namespace {
+using valiant::testlib::JsonRpcGetIntegerParam;
+
+#if defined(IMAGE_SERVER_ETHERNET)
+char* get_ethernet_ip(struct netif* ethernet) {
+    while (true) {
+        auto* dhcp = netif_dhcp_data(ethernet);
+        if (dhcp->state == DHCP_STATE_BOUND) {
+            break;
+        }
+        taskYIELD();
+    }
+    return ip4addr_ntoa(netif_ip4_addr(ethernet));
+}
+#endif  // defined(IMAGE_SERVER_ETHERNET)
+
+void get_image_from_camera(struct jsonrpc_request* request) {
+    int width, height;
+    if (!JsonRpcGetIntegerParam(request, "width", &width)) {
+        return;
+    }
+    if (!JsonRpcGetIntegerParam(request, "height", &height)) {
+        return;
+    }
+
+    valiant::CameraTask::GetSingleton()->SetPower(true);
+    valiant::CameraTask::GetSingleton()->Enable(
+        valiant::camera::Mode::STREAMING);
+    std::vector<uint8_t> image(width * height * /*channels=*/3);
+    valiant::camera::FrameFormat fmt{valiant::camera::Format::RGB, width,
+                                     height, false, image.data()};
+    auto ret = valiant::CameraTask::GetFrame({fmt});
+    valiant::CameraTask::GetSingleton()->Disable();
+    valiant::CameraTask::GetSingleton()->SetPower(false);
+
+    if (!ret) {
+        jsonrpc_return_error(request, -1, "Failed to get image from camera.",
+                             nullptr);
+        return;
+    }
+    jsonrpc_return_success(request, "{%Q: %d, %Q: %d, %Q: %V}", "width", width,
+                           "height", height, "base64_data", image.size(),
+                           image.data());
+}
+
+}  // namespace
+
+extern "C" void app_main(void* param) {
+#if defined(IMAGE_SERVER_ETHERNET)
+    valiant::InitializeEthernet(true);
+    auto* ethernet = valiant::GetEthernetInterface();
+    if (!ethernet) {
+        printf("Unable to bring up ethernet...\r\n");
+        vTaskSuspend(nullptr);
+    }
+    auto* ethernet_ip = get_ethernet_ip(ethernet);
+    printf("Starting Image RPC Server on: %s\r\n", ethernet_ip);
+    jsonrpc_init(nullptr, ethernet_ip);
+    jsonrpc_export("get_ethernet_ip", [](struct jsonrpc_request* request) {
+        jsonrpc_return_success(
+            request, "{%Q: %Q}", "ethernet_ip",
+            reinterpret_cast<char*>(request->ctx->response_cb_data));
+    });
+#else
+    printf("Starting Image RPC Server...\r\n");
+    jsonrpc_init(nullptr, nullptr);
+#endif  // defined(IMAGE_SERVER_ETHERNET)
+    jsonrpc_export("get_image_from_camera", get_image_from_camera);
+    valiant::UseHttpServer(new valiant::JsonRpcHttpServer);
+    printf("Server started...\r\n");
+    vTaskSuspend(nullptr);
+}
---
+++ apps/ImageServer/image_server_test.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3
+
+import argparse
+import base64
+import inspect
+import json
+import requests
+import itertools
+from PIL import Image
+
+parser = argparse.ArgumentParser(description='Image server client')
+parser.add_argument('--usb_ip', type=str, required=True, help='Valiant USB IP address.')
+parser.add_argument('--ethernet', action='store_true', help='Get image from ethernet ip.')
+parser.add_argument('--ethernet_ip', type=str, required=False, help='Valiant Ethernet IP address.')
+parser.add_argument('--image_width', type=int, default=700, help="Specify image width.")
+parser.add_argument('--image_height', type=int, default=700, help="Specify image height.")
+
+rpc_id = itertools.count()
+
+
+def rpc(func):
+  def rpc_impl(*args, **kwargs):
+    params = inspect.getcallargs(func, *args, **kwargs)
+    url = f"http://{params.pop('ip')}:80/jsonrpc"
+    payload = {
+      'id': next(rpc_id),
+      'jsonrpc': '2.0',
+      'method': func.__name__,
+      'params': [params or {}],
+    }
+    print(f'url: {url}')
+    print(f'payload:\n{json.dumps(payload, indent=2)}')
+    return requests.post(url, json=payload).json()
+
+  return rpc_impl
+
+
+@rpc
+def get_ethernet_ip(ip):
+  """Get ethernet ip address.
+  Args:
+    ip: the usb ip address.
+  Returns:
+    A JSON-RPC result packet with the ethernet ip address.
+    Example:
+       {'id': 24, 'result': {'ethernet_ip': '192.168.1.117'}}
+  """
+
+
+@rpc
+def get_image_from_camera(ip, width, height):
+  """Get image from camera.
+  Args:
+    width:
+    height: A boolean value representing the desired rail stat
+  Returns:
+    A JSON-RPC result packet with no extra data, or JSON-RPC error.
+    Example:
+      {'id': 1, 'result': {}}
+  """
+
+
+def display_image(response, width, height):
+  result = get_field_or_die(response, 'result')
+  image_data_base64 = get_field_or_die(result, 'base64_data')
+  image_data = base64.b64decode(image_data_base64)
+  im = Image.frombytes('RGB', (width, height), image_data, 'raw')
+  im.show()
+
+
+def parse_ethernet_ip(response):
+  result = get_field_or_die(response, 'result')
+  return get_field_or_die(result, 'ethernet_ip')
+
+
+def get_field_or_die(data, field_name):
+  if field_name not in data:
+    print(f'Unable to parse {field_name} from data: {data}\r\n')
+    exit(1)
+  return data[field_name]
+
+
+def main():
+  args = parser.parse_args()
+  width = args.image_width
+  height = args.image_height
+
+  if args.ethernet:
+    ethernet_ip = args.ethernet_ip
+    if ethernet_ip is None:
+      ethernet_ip = parse_ethernet_ip(get_ethernet_ip(args.usb_ip))
+    print(f'Ethernet ip: {ethernet_ip}')
+    display_image(get_image_from_camera(ethernet_ip, width, height), width, height)
+  else:  # USB
+    display_image(get_image_from_camera(args.usb_ip, width, height), width, height)
+
+
+if __name__ == '__main__':
+  main()
