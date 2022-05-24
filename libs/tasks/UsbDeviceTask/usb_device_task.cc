6bef931616c49fb2145218bce94a2b52d9b4127e
Dmitry Kovalev <dkovalev@google.com>
2022-05-24 10:39:53 -0700

    Format usb_device_task files
    Change-Id: Ic0ced236b9ab5430fba27a5fda7aa2ed3bc02211
    GitOrigin-RevId: 0a94c5e6601ce6e8ce34e750c3e641a11bac6b69

--- libs/tasks/UsbDeviceTask/usb_device_task.cc
+++ libs/tasks/UsbDeviceTask/usb_device_task.cc
@@ -1,15 +1,15 @@
-#include "libs/base/utils.h"
-#include "libs/nxp/rt1176-sdk/board.h"
-#include "libs/nxp/rt1176-sdk/clock_config.h"
-#include "libs/nxp/rt1176-sdk/usb_device_config.h"
 #include "libs/tasks/UsbDeviceTask/usb_device_task.h"
-#include "third_party/nxp/rt1176-sdk/middleware/usb/device/usb_device.h"
-#include "third_party/nxp/rt1176-sdk/middleware/usb/phy/usb_phy.h"
 
 #include <cstdio>
 
+#include "libs/base/utils.h"
+#include "libs/nxp/rt1176-sdk/board.h"
+#include "libs/nxp/rt1176-sdk/clock_config.h"
+#include "third_party/nxp/rt1176-sdk/middleware/usb/phy/usb_phy.h"
+
 extern "C" void USB_OTG1_IRQHandler(void) {
-    USB_DeviceEhciIsrFunction(coral::micro::UsbDeviceTask::GetSingleton()->device_handle());
+    USB_DeviceEhciIsrFunction(
+        coral::micro::UsbDeviceTask::GetSingleton()->device_handle());
 }
 
 namespace coral::micro {
@@ -21,7 +21,7 @@ constexpr int kUSBControllerId = kUSB_ControllerEhci0;
 //
 // Expects param->buffer to be set to a suitable output buffer
 void ToUsbStringDescriptor(const char* s,
-        usb_device_get_string_descriptor_struct_t* param) {
+                           usb_device_get_string_descriptor_struct_t* param) {
     uint8_t* output = param->buffer;
 
     // prefix is size and type
@@ -37,11 +37,14 @@ void ToUsbStringDescriptor(const char* s,
 }
 }  // namespace
 
-usb_status_t UsbDeviceTask::StaticHandler(usb_device_handle device_handle, uint32_t event, void *param) {
-    return coral::micro::UsbDeviceTask::GetSingleton()->Handler(device_handle, event, param);
+usb_status_t UsbDeviceTask::StaticHandler(usb_device_handle device_handle,
+                                          uint32_t event, void* param) {
+    return coral::micro::UsbDeviceTask::GetSingleton()->Handler(device_handle,
+                                                                event, param);
 }
 
-usb_status_t UsbDeviceTask::Handler(usb_device_handle device_handle, uint32_t event, void *param) {
+usb_status_t UsbDeviceTask::Handler(usb_device_handle device_handle,
+                                    uint32_t event, void* param) {
     static uint8_t string_buffer[64];
     bool event_ret = true;
     usb_status_t ret = kStatus_USB_Error;
@@ -50,29 +53,35 @@ usb_status_t UsbDeviceTask::Handler(usb_device_handle device_handle, uint32_t ev
             ret = kStatus_USB_Success;
             break;
         case kUSB_DeviceEventGetDeviceDescriptor: {
-            auto* device_desc = static_cast<usb_device_get_device_descriptor_struct_t*>(param);
-            device_desc->buffer = const_cast<uint8_t*>(reinterpret_cast<const uint8_t*>(&device_descriptor_));
+            auto* device_desc =
+                static_cast<usb_device_get_device_descriptor_struct_t*>(param);
+            device_desc->buffer = const_cast<uint8_t*>(
+                reinterpret_cast<const uint8_t*>(&device_descriptor_));
             device_desc->length = sizeof(device_descriptor_);
             ret = kStatus_USB_Success;
             break;
         }
         case kUSB_DeviceEventGetConfigurationDescriptor: {
-            auto* config_desc = static_cast<usb_device_get_configuration_descriptor_struct_t*>(param);
+            auto* config_desc =
+                static_cast<usb_device_get_configuration_descriptor_struct_t*>(
+                    param);
             config_desc->buffer = composite_descriptor_.data();
             config_desc->length = composite_descriptor_.size();
             ret = kStatus_USB_Success;
             break;
         }
         case kUSB_DeviceEventGetStringDescriptor: {
-            auto* string_desc = static_cast<usb_device_get_string_descriptor_struct_t*>(param);
+            auto* string_desc =
+                static_cast<usb_device_get_string_descriptor_struct_t*>(param);
             string_desc->buffer = string_buffer;
             if (string_desc->stringIndex == 0) {
                 string_desc->length = sizeof(lang_id_desc_);
-                memcpy(string_desc->buffer, &lang_id_desc_, sizeof(lang_id_desc_));
+                memcpy(string_desc->buffer, &lang_id_desc_,
+                       sizeof(lang_id_desc_));
                 ret = kStatus_USB_Success;
                 break;
             }
-            if (string_desc->languageId != 0x0409) { // English
+            if (string_desc->languageId != 0x0409) {  // English
                 ret = kStatus_USB_InvalidRequest;
                 break;
             }
@@ -90,7 +99,8 @@ usb_status_t UsbDeviceTask::Handler(usb_device_handle device_handle, uint32_t ev
                     ret = kStatus_USB_Success;
                     break;
                 default:
-                    printf("Unhandled string request: %d\r\n", string_desc->stringIndex);
+                    printf("Unhandled string request: %d\r\n",
+                           string_desc->stringIndex);
                     ret = kStatus_USB_InvalidRequest;
                     break;
             }
@@ -126,7 +136,7 @@ usb_status_t UsbDeviceTask::Handler(usb_device_handle device_handle, uint32_t ev
 void UsbDeviceTask::AddDevice(const usb_device_class_config_struct_t& config,
                               usb_set_handle_callback sh_cb,
                               usb_handle_event_callback he_cb,
-                              const void *descriptor_data,
+                              const void* descriptor_data,
                               size_t descriptor_data_size) {
     configs_.push_back(config);
     set_handle_callbacks_.push_back(sh_cb);
@@ -134,15 +144,19 @@ void UsbDeviceTask::AddDevice(const usb_device_class_config_struct_t& config,
 
     size_t old_size = composite_descriptor_size_;
     composite_descriptor_size_ += descriptor_data_size;
-    memcpy(composite_descriptor_.data() + old_size, descriptor_data, descriptor_data_size);
+    memcpy(composite_descriptor_.data() + old_size, descriptor_data,
+           descriptor_data_size);
 
-    auto* p_composite_descriptor = reinterpret_cast<CompositeDescriptor*>(composite_descriptor_.data());
+    auto* p_composite_descriptor =
+        reinterpret_cast<CompositeDescriptor*>(composite_descriptor_.data());
     p_composite_descriptor->conf.total_length = composite_descriptor_size_;
 }
 
 bool UsbDeviceTask::Init() {
-    config_list_ = { configs_.data(), &UsbDeviceTask::StaticHandler, static_cast<uint8_t>(configs_.size()) };
-    usb_status_t ret = USB_DeviceClassInit(kUSBControllerId, &config_list_, &device_handle_);
+    config_list_ = {configs_.data(), &UsbDeviceTask::StaticHandler,
+                    static_cast<uint8_t>(configs_.size())};
+    usb_status_t ret =
+        USB_DeviceClassInit(kUSBControllerId, &config_list_, &device_handle_);
     if (ret != kStatus_USB_Success) {
         printf("USB_DeviceClassInit failed %d\r\n", ret);
         return false;
@@ -174,19 +188,21 @@ UsbDeviceTask::UsbDeviceTask() {
     NVIC_SetPriority(irq_number, 6);
     NVIC_EnableIRQ(irq_number);
     constexpr CompositeDescriptor composite_descriptor = {
-        .conf = {
-            .length = sizeof(ConfigurationDescriptor),
-            .descriptor_type = 0x02,
-            .total_length = sizeof(CompositeDescriptor),
-            .num_interfaces = 0, // Managed by next_interface_value
-            .config_value = 1,
-            .config = 0,
-            .attributes = 0x80, // kUsb11AndHigher
-            .max_power = 250, // kUses500mA
-        },
+        .conf =
+            {
+                .length = sizeof(ConfigurationDescriptor),
+                .descriptor_type = 0x02,
+                .total_length = sizeof(CompositeDescriptor),
+                .num_interfaces = 0,  // Managed by next_interface_value
+                .config_value = 1,
+                .config = 0,
+                .attributes = 0x80,  // kUsb11AndHigher
+                .max_power = 250,    // kUses500mA
+            },
     };
     composite_descriptor_size_ = sizeof(composite_descriptor);
-    memcpy(composite_descriptor_.data(), &composite_descriptor, sizeof(composite_descriptor));
+    memcpy(composite_descriptor_.data(), &composite_descriptor,
+           sizeof(composite_descriptor));
 }
 
 void UsbDeviceTask::UsbDeviceTaskFn() {
--- libs/tasks/UsbDeviceTask/usb_device_task.h
+++ libs/tasks/UsbDeviceTask/usb_device_task.h
@@ -1,28 +1,32 @@
 #ifndef _LIBS_TASKS_USBDEVICETASK_USBDEVICETASK_H_
 #define _LIBS_TASKS_USBDEVICETASK_USBDEVICETASK_H_
 
-#include "libs/usb/descriptors.h"
-#include "third_party/nxp/rt1176-sdk/middleware/usb/include/usb.h"
-#include "third_party/nxp/rt1176-sdk/middleware/usb/device/usb_device.h"
-#include "third_party/nxp/rt1176-sdk/middleware/usb/output/source/device/class/usb_device_class.h"
-
 #include <array>
 #include <functional>
 #include <string>
 #include <vector>
 
+#include "libs/nxp/rt1176-sdk/usb_device_config.h"  // must be before usb_device.h
+#include "libs/usb/descriptors.h"
+#include "third_party/nxp/rt1176-sdk/middleware/usb/device/usb_device.h"
+#include "third_party/nxp/rt1176-sdk/middleware/usb/include/usb.h"
+#include "third_party/nxp/rt1176-sdk/middleware/usb/output/source/device/class/usb_device_class.h"
+
 namespace coral::micro {
 
 class UsbDeviceTask {
-  public:
+   public:
     using usb_set_handle_callback = std::function<void(class_handle_t)>;
     using usb_handle_event_callback = std::function<bool(uint32_t, void*)>;
 
     UsbDeviceTask();
     UsbDeviceTask(const UsbDeviceTask&) = delete;
-    UsbDeviceTask &operator=(const UsbDeviceTask&) = delete;
+    UsbDeviceTask& operator=(const UsbDeviceTask&) = delete;
 
-    void AddDevice(const usb_device_class_config_struct_t& config, usb_set_handle_callback sh_cb, usb_handle_event_callback he_cb, const void* descriptor_data, size_t descriptor_data_size);
+    void AddDevice(const usb_device_class_config_struct_t& config,
+                   usb_set_handle_callback sh_cb,
+                   usb_handle_event_callback he_cb, const void* descriptor_data,
+                   size_t descriptor_data_size);
     bool Init();
     static UsbDeviceTask* GetSingleton() {
         static UsbDeviceTask task;
@@ -32,13 +36,16 @@ class UsbDeviceTask {
 
     uint8_t next_descriptor_value() { return ++next_descriptor_value_; }
     uint8_t next_interface_value() {
-      uint8_t next_interface = next_interface_value_;
-      CompositeDescriptor *p_composite_descriptor = reinterpret_cast<CompositeDescriptor*>(composite_descriptor_.data());
-      p_composite_descriptor->conf.num_interfaces = ++next_interface_value_;
-      return next_interface;
+        uint8_t next_interface = next_interface_value_;
+        CompositeDescriptor* p_composite_descriptor =
+            reinterpret_cast<CompositeDescriptor*>(
+                composite_descriptor_.data());
+        p_composite_descriptor->conf.num_interfaces = ++next_interface_value_;
+        return next_interface;
     }
     usb_device_handle device_handle() const { return device_handle_; }
-  private:
+
+   private:
     uint8_t next_descriptor_value_ = 0;
     uint8_t next_interface_value_ = 0;
 
@@ -60,8 +67,7 @@ class UsbDeviceTask {
         .manufacturer = 1,
         .product = 2,
         .serial_number = 3,
-        .num_configurations = 1
-    };
+        .num_configurations = 1};
     std::array<uint8_t, 1024> composite_descriptor_;
     size_t composite_descriptor_size_ = 0;
 
@@ -71,8 +77,10 @@ class UsbDeviceTask {
         .lang_id = 0x0409,
     };
 
-    static usb_status_t StaticHandler(usb_device_handle device_handle, uint32_t event, void *param);
-    usb_status_t Handler(usb_device_handle device_handle, uint32_t event, void *param);
+    static usb_status_t StaticHandler(usb_device_handle device_handle,
+                                      uint32_t event, void* param);
+    usb_status_t Handler(usb_device_handle device_handle, uint32_t event,
+                         void* param);
 
     std::vector<usb_device_class_config_struct_t> configs_;
     usb_device_class_config_list_struct_t config_list_;
