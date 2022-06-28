f3356caab7c9063e37ec4c74a3bc7c3a073849d1
Scott Main <smain@google.com>
2022-06-28 14:41:10 -0700

    Add example for user button callback.
    This also removes blink_led_m7 because it has not unique value
    when compared to this and the m4 version.
    
    Change-Id: I2e66832d28b1e03e436f54067201544db2a77a7c
    GitOrigin-RevId: c4f0538abc76ab8052b84fc0f94be0918247542b

---
+++ examples/button_led/button_led.cc
@@ -0,0 +1,38 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cstdio>
+
+#include "libs/base/gpio.h"
+#include "libs/base/led.h"
+#include "third_party/freertos_kernel/include/FreeRTOS.h"
+#include "third_party/freertos_kernel/include/task.h"
+
+// Toggles the User LED in response to button presses
+
+bool user_led_on = false;
+
+// Callback for user button
+void OnButtonPressed() {
+    user_led_on = !user_led_on;
+    coral::micro::led::Set(coral::micro::led::LED::kUser, user_led_on);
+}
+
+extern "C" void app_main(void* param) {
+    printf("Press the user button.\r\n");
+
+    // Register callback for the user button
+    coral::micro::gpio::RegisterIRQHandler(coral::micro::gpio::Gpio::kUserButton, OnButtonPressed);
+    vTaskSuspend(nullptr);
+}
