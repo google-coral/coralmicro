fd9f1dafc6608c0908ca78c8fb95e882580f065c
Scott Main <smain@google.com>
2022-07-01 09:54:53 -0700

    Simplify/rename LED examples and remove redundant LED examples.
    We should not encourage modifying the Edge TPU LED behavior, so
    I removed that one from the Blink example.
    
    Change-Id: Ida152f9c76f12865eb7421cc7f8f70554c6a753e
    GitOrigin-RevId: b07495fe23f062ca43705dd0ca510e1396b5b132

---
+++ sketches/BlinkLED/BlinkLED.ino
@@ -0,0 +1,19 @@
+/*
+  Blinks the user LED (green) and status LED (orange).
+*/
+
+#include "Arduino.h"
+
+void setup() {
+    pinMode(PIN_LED_USER, OUTPUT);
+    pinMode(PIN_LED_STATUS, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(PIN_LED_USER, HIGH);
+    digitalWrite(PIN_LED_STATUS, HIGH);
+    delay(1000);
+    digitalWrite(PIN_LED_USER, LOW);
+    digitalWrite(PIN_LED_STATUS, LOW);
+    delay(1000);
+}
---
+++ sketches/ButtonLED/ButtonLED.ino
@@ -0,0 +1,20 @@
+/*
+  Turns the user LED (green) on when you press the user button
+  on the Coral Dev Board Micro.
+*/
+
+#include "Arduino.h"
+
+int ledPin = PIN_LED_USER;
+int buttonPin = PIN_BTN;
+PinStatus val = LOW;
+
+void setup() {
+    pinMode(ledPin, OUTPUT);
+    pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+    val = digitalRead(buttonPin);
+    digitalWrite(ledPin, val == LOW ? HIGH : LOW);
+}
