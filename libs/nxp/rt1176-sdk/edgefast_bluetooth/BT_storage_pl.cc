6851dd8c9531bb07508555c7c1c7542f2929653d
Dmitry Kovalev <dkovalev@google.com>
2022-07-06 14:43:49 -0700

    Use LFS API directly in BT_storage_pl.cc
    Change-Id: I3a9f067efe2886bb9aaecba0566975bfa6038900
    GitOrigin-RevId: 00defe08b2d2d8f29e3b52e94fad5f868ba76670

--- libs/nxp/rt1176-sdk/edgefast_bluetooth/BT_storage_pl.cc
+++ libs/nxp/rt1176-sdk/edgefast_bluetooth/BT_storage_pl.cc
@@ -1,20 +1,28 @@
 // Port of
 // third_party/nxp/rt1176-sdk/middleware/wireless/ethermind/port/pal/mcux/bluetooth/BT_storage_pl.c
-// to use Dev Board Micro's filesystem APIs.
 
 #include "libs/base/filesystem.h"
+
+/*
+ *  Copyright (C) 2013. Mindtree Ltd.
+ *  All rights reserved.
+ */
+
+using coralmicro::filesystem::Lfs;
 
 extern "C" {
 
 #include "third_party/nxp/rt1176-sdk/middleware/wireless/ethermind/port/pal/mcux/bluetooth/BT_storage_pl.h"
 
-static lfs_file_t *fp[STORAGE_NUM_TYPES];
+/* Storage File Handle array */
+static lfs_file_t* fp[STORAGE_NUM_TYPES];
 static lfs_file_t lfs_file[STORAGE_NUM_TYPES];
+
 /* Storage File Name array */
-static UCHAR *fn[STORAGE_NUM_TYPES] = {
-    (UCHAR *)"btps.db",
+static UCHAR* fn[STORAGE_NUM_TYPES] = {
+    (UCHAR*)"btps.db",
 #ifdef STORAGE_RETENTION_SUPPORT
-    (UCHAR *)"btrn.db",
+    (UCHAR*)"btrn.db",
 #endif /* STORAGE_RETENTION_SUPPORT */
 };
 
@@ -24,46 +32,46 @@ static UCHAR ssign[STORAGE_NUM_TYPES][STORAGE_SKEY_SIZE] = {
 
 API_RESULT storage_open_pl(UCHAR type, UCHAR mode) {
   if (NULL != fp[type]) {
-    coralmicro::filesystem::Close(fp[type]);
+    (void)lfs_file_close(Lfs(), fp[type]);
     fp[type] = NULL;
   }
-  bool open;
-  if (mode == STORAGE_OPEN_MODE_WRITE) {
-    open = coralmicro::filesystem::Open(&lfs_file[type], (char *)fn[type], true);
-  } else {
-    open = coralmicro::filesystem::Open(&lfs_file[type], (char *)fn[type]);
-  }
-  if (!open) {
+  /* Set the file access mode */
+  int rw =
+      (int)((STORAGE_OPEN_MODE_WRITE == mode) ? (LFS_O_WRONLY | LFS_O_CREAT)
+                                              : LFS_O_RDONLY);
+
+  int err = lfs_file_open(Lfs(), &lfs_file[type], (CHAR*)fn[type], rw);
+
+  if (err < 0) {
     return API_FAILURE;
   }
 
   fp[type] = &lfs_file[type];
+
   return API_SUCCESS;
 }
 
 API_RESULT storage_close_pl(UCHAR type, UCHAR mode) {
+  BT_IGNORE_UNUSED_PARAM(mode);
   if (NULL != fp[type]) {
-    coralmicro::filesystem::Close(fp[type]);
+    (void)lfs_file_close(Lfs(), fp[type]);
     fp[type] = NULL;
   }
-
   return API_SUCCESS;
 }
 
-INT16 storage_write_pl(UCHAR type, void *buffer, UINT16 size) {
-  INT16 nbytes;
-  nbytes = 0;
-
+INT16 storage_write_pl(UCHAR type, void* buffer, UINT16 size) {
+  INT16 nbytes = 0;
   if (NULL != fp[type]) {
-    nbytes = (INT16)coralmicro::filesystem::Write(fp[type], buffer, size);
+    nbytes = (INT16)lfs_file_write(Lfs(), fp[type], buffer, size);
   }
   return nbytes;
 }
 
-INT16 storage_read_pl(UCHAR type, void *buffer, UINT16 size) {
+INT16 storage_read_pl(UCHAR type, void* buffer, UINT16 size) {
   INT16 nbytes = 0;
   if (NULL != fp[type]) {
-    nbytes = (INT16)coralmicro::filesystem::Read(fp[type], buffer, size);
+    nbytes = (INT16)lfs_file_read(Lfs(), fp[type], buffer, size);
   }
   return nbytes;
 }
@@ -71,8 +79,8 @@ INT16 storage_read_pl(UCHAR type, void *buffer, UINT16 size) {
 INT16 storage_write_signature_pl(UCHAR type) {
   INT16 nbytes = 0;
   if (NULL != fp[type]) {
-    nbytes = (INT16)coralmicro::filesystem::Write(fp[type], ssign[type],
-                                               STORAGE_SKEY_SIZE);
+    nbytes =
+        (INT16)lfs_file_write(Lfs(), fp[type], ssign[type], STORAGE_SKEY_SIZE);
   }
   return nbytes;
 }
@@ -80,8 +88,10 @@ INT16 storage_write_signature_pl(UCHAR type) {
 INT16 storage_read_signature_pl(UCHAR type) {
   INT16 nbytes = 0;
   UCHAR sign[STORAGE_SKEY_SIZE];
+
   if (NULL != fp[type]) {
-    nbytes = coralmicro::filesystem::Read(fp[type], sign, STORAGE_SKEY_SIZE);
+    nbytes = lfs_file_read(Lfs(), fp[type], sign, STORAGE_SKEY_SIZE);
+
     if (BT_mem_cmp(ssign[type], sign, STORAGE_SKEY_SIZE)) {
       return -1;
     }
@@ -89,9 +99,18 @@ INT16 storage_read_signature_pl(UCHAR type) {
   return nbytes;
 }
 
-void storage_bt_init_pl(void) {
-  for (unsigned int i = 0; i < STORAGE_NUM_TYPES; ++i) {
+void storage_bt_init_pl() {
+  for (UCHAR i = 0; i < STORAGE_NUM_TYPES; i++) {
     fp[i] = NULL;
+  }
+}
+
+void storage_bt_shutdown_pl() {
+  for (UCHAR i = 0; i < STORAGE_NUM_TYPES; i++) {
+    if (NULL != fp[i]) {
+      lfs_file_close(Lfs(), fp[i]);
+      fp[i] = NULL;
+    }
   }
 }
 
