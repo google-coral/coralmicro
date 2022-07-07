136dc0fe1ef264c0b1bacc70b2ee1e31bba12b94
Michael Brooks <michaelbrooks@google.com>
2022-07-07 13:26:03 -0700

    Add Camera Classification Example, Bayered Model
    * Adds a camera_classify, which is very similar to the detection_camera
      example (but for classification).
    * Adds a new MNv2 model which has been trained on bayered data at the
      resolution of our camera (324x324). This allows the model to be
      directly feed the output of the camera.
    * Uses the bayered model in the new classify_camera. The image is
      demosaiced on the host (with OpenCV) before shown.
    
    Tested:
    classify_camera
    
    Change-Id: Ibebb96130afce74e2c6777ba4f32199e07516e1c
    GitOrigin-RevId: 36e33ebacf43752c04d1dd234191ef26ffe5954d

---
+++ examples/classify_camera/classify_camera.cc
@@ -0,0 +1,157 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cstring>
+#include <vector>
+
+#include "libs/base/filesystem.h"
+#include "libs/camera/camera.h"
+#include "libs/rpc/rpc_http_server.h"
+#include "libs/tensorflow/classification.h"
+#include "libs/tensorflow/utils.h"
+#include "libs/tpu/edgetpu_manager.h"
+#include "libs/tpu/edgetpu_op.h"
+#include "third_party/freertos_kernel/include/FreeRTOS.h"
+#include "third_party/freertos_kernel/include/task.h"
+#include "third_party/mjson/src/mjson.h"
+#include "third_party/tflite-micro/tensorflow/lite/micro/micro_error_reporter.h"
+#include "third_party/tflite-micro/tensorflow/lite/micro/micro_interpreter.h"
+#include "third_party/tflite-micro/tensorflow/lite/micro/micro_mutable_op_resolver.h"
+
+// Runs a local server with an endpoint called 'classification_from_camera',
+// which will capture an image from the board's camera, run the image through a
+// classification model and return the results in a JSON response.
+//
+// The response includes only the top result with a JSON file like this:
+//
+// {
+// 'id': int,
+// 'result':
+//     {
+//     'width': int,
+//     'height': int,
+//     'base64_data': image_bytes,
+//     'bayered': bayered,
+//     'id': id,
+//     'score': score,
+// }
+
+namespace {
+constexpr char kModelPath[] = "/models/mnv2_324_quant_bayered_edgetpu.tflite";
+constexpr int kTensorArenaSize = 8 * 1024 * 1024;
+STATIC_TENSOR_ARENA_IN_SDRAM(tensor_arena, kTensorArenaSize);
+
+void ClassifyFromCamera(struct jsonrpc_request* r) {
+    auto* interpreter =
+        reinterpret_cast<tflite::MicroInterpreter*>(r->ctx->response_cb_data);
+
+    auto* input_tensor = interpreter->input_tensor(0);
+    int model_height = input_tensor->dims->data[1];
+    int model_width = input_tensor->dims->data[2];
+
+    coralmicro::CameraTask::GetSingleton()->SetPower(true);
+    coralmicro::CameraTask::GetSingleton()->Enable(
+        coralmicro::camera::Mode::kStreaming);
+
+    // If the model name includes "bayered", provide the raw datastream from the
+    // camera.
+    bool bayered = strstr(kModelPath, "bayered");
+    std::vector<uint8_t> image(model_width * model_height *
+                               /*channels=*/(bayered ? 1 : 3));
+    auto format = bayered ? coralmicro::camera::Format::kRaw
+                          : coralmicro::camera::Format::kRgb;
+    coralmicro::camera::FrameFormat fmt{
+        format,
+        coralmicro::camera::FilterMethod::kBilinear,
+        coralmicro::camera::Rotation::k0,
+        model_width,
+        model_height,
+        false,
+        image.data()};
+
+    // Discard the first frame to ensure no power-on artifacts exist.
+    bool ret = coralmicro::CameraTask::GetFrame({fmt});
+    ret = coralmicro::CameraTask::GetFrame({fmt});
+
+    coralmicro::CameraTask::GetSingleton()->Disable();
+    coralmicro::CameraTask::GetSingleton()->SetPower(false);
+
+    if (!ret) {
+        jsonrpc_return_error(r, -1, "Failed to get image from camera.",
+                             nullptr);
+        return;
+    }
+
+    std::memcpy(tflite::GetTensorData<uint8_t>(input_tensor), image.data(),
+                image.size());
+
+    if (interpreter->Invoke() != kTfLiteOk) {
+        jsonrpc_return_error(r, -1, "Invoke failed", nullptr);
+        return;
+    }
+
+    auto results =
+        coralmicro::tensorflow::GetClassificationResults(interpreter, 0.0f, 1);
+    if (!results.empty()) {
+        const auto& result = results[0];
+        jsonrpc_return_success(
+            r, "{%Q: %d, %Q: %d, %Q: %V, %Q: %d, %Q: %d, %Q: %g}", "width",
+            model_width, "height", model_height, "base64_data", image.size(),
+            image.data(), "bayered", bayered, "id", result.id, "score",
+            result.score);
+        return;
+    }
+    jsonrpc_return_success(r, "{%Q: %d, %Q: %d, %Q: %V, %Q: %d}", "width",
+                           model_width, "height", model_height, "base64_data",
+                           image.size(), image.data(), "bayered", bayered);
+}
+}  // namespace
+
+extern "C" void app_main(void* param) {
+    std::vector<uint8_t> model;
+    if (!coralmicro::filesystem::ReadFile(kModelPath, &model)) {
+        printf("ERROR: Failed to load %s\r\n", kModelPath);
+        vTaskSuspend(nullptr);
+    }
+
+    auto tpu_context = coralmicro::EdgeTpuManager::GetSingleton()->OpenDevice();
+    if (!tpu_context) {
+        printf("ERROR: Failed to get EdgeTpu context\r\n");
+        vTaskSuspend(nullptr);
+    }
+
+    tflite::MicroErrorReporter error_reporter;
+    tflite::MicroMutableOpResolver<1> resolver;
+    resolver.AddCustom(coralmicro::kCustomOp, coralmicro::RegisterCustomOp());
+
+    tflite::MicroInterpreter interpreter(tflite::GetModel(model.data()),
+                                         resolver, tensor_arena,
+                                         kTensorArenaSize, &error_reporter);
+    if (interpreter.AllocateTensors() != kTfLiteOk) {
+        printf("ERROR: AllocateTensors() failed\r\n");
+        vTaskSuspend(nullptr);
+    }
+
+    if (interpreter.inputs().size() != 1) {
+        printf("ERROR: Model must have only one input tensor\r\n");
+        vTaskSuspend(nullptr);
+    }
+
+    printf("Initializing classification server...%p\r\n", &interpreter);
+    jsonrpc_init(nullptr, &interpreter);
+    jsonrpc_export("classify_from_camera", ClassifyFromCamera);
+    coralmicro::UseHttpServer(new coralmicro::JsonRpcHttpServer);
+    printf("Classification server ready!\r\n");
+    vTaskSuspend(nullptr);
+}
---
+++ examples/classify_camera/classify_camera_client.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import base64
+import re
+import requests
+
+from PIL import Image, ImageDraw
+
+"""
+Displays classification results received from the classify_camera server
+running on a connected Dev Board Micro.
+
+First load classify_camera example onto the Dev Board Micro:
+
+    python3 scripts/flashtool.py -b build -e classify_camera
+
+Then start this client on your computer:
+
+    python3 examples/classify_camera/classify_camera_client.py
+
+You should see the image result appear in a new window.
+
+Note that this example shows only the top classification result.
+"""
+
+
+def read_label_file(file_path):
+    """Reads labels from a text file and returns it as a dictionary.
+    This function supports label files with the following formats:
+    + Each line contains id and description separated by colon or space.
+      Example: ``0:cat`` or ``0 cat``.
+    + Each line contains a description only. The returned label id's are based on
+      the row number.
+    Args:
+      file_path (str): path to the label file.
+    Returns:
+      Dict of (int, string) which maps label id to description.
+    """
+    with open(file_path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    ret = {}
+    for row_number, content in enumerate(lines):
+        pair = re.split(r'[:\s]+', content.strip(), maxsplit=1)
+        if len(pair) == 2 and pair[0].strip().isdigit():
+            ret[int(pair[0])] = pair[1].strip()
+        else:
+            ret[row_number] = content.strip()
+    return ret
+
+
+def get_field_or_die(data, field_name):
+    if field_name not in data:
+        print(f'Unable to parse {field_name} from data: {data}\r\n')
+        exit(1)
+    return data[field_name]
+
+
+def main():
+    url = "http://10.10.10.1:80/jsonrpc"
+
+    payload = {
+        "method": "classify_from_camera",
+        "jsonrpc": "2.0",
+        "id": 0,
+    }
+
+    # Unpack the JSON result
+    response = requests.post(url, json=payload).json()
+    result = get_field_or_die(response, 'result')
+
+    # Get the image size
+    width = get_field_or_die(result, 'width')
+    height = get_field_or_die(result, 'height')
+
+    # Decode the image data
+    image_data_base64 = get_field_or_die(result, 'base64_data')
+    image_data = base64.b64decode(image_data_base64)
+    bayered = get_field_or_die(result, 'bayered')
+    if bayered:
+        import cv2
+        import numpy as np
+        np_data = np.frombuffer(image_data, dtype=np.uint8)
+        np_data = np_data.reshape(width, height)
+        debayered = cv2.cvtColor(np_data, cv2.COLOR_BAYER_BG2BGR)
+        im = Image.fromarray(debayered)
+    else:
+        im = Image.frombytes('RGB', (width, height), image_data, 'raw')
+
+    # Display the image with the classification result.
+    draw = ImageDraw.Draw(im)
+    labels = read_label_file('models/imagenet_labels.txt')
+    if 'id' in result:
+        text = f'{labels[result["id"]]}\nScore: {result["score"]}'
+        print(text)
+        draw.text((0, 0), text)
+
+    im.show()
+
+
+if __name__ == '__main__':
+    main()
