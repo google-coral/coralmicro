969bc24ed6f58e30f3f705a2d68a5a18abcbb1ea
Scott Main <smain@google.com>
2022-07-07 18:27:48 -0700

    split freertos_hello_world into an example for tasks and for console read/write.
    bug: 238367620
    
    Change-Id: I4d2d2426cf746004e1a3975aeac659a21b6db6d5
    GitOrigin-RevId: d4b27d0b6f58efb2cb01f1f41a620c7828f6f80d

---
+++ examples/console_read_write/console_read_write.cc
@@ -0,0 +1,33 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cstdio>
+
+#include "libs/base/console_m7.h"
+#include "libs/base/tasks.h"
+
+// Reads character input from the serial console and writes it back.
+
+extern "C" void app_main(void* param) {
+    printf("Type into the serial console.\r\n");
+
+    char ch;
+    while(true) {
+        int bytes = coralmicro::ConsoleM7::GetSingleton()->Read(&ch, 1);
+        if (bytes == 1) {
+            coralmicro::ConsoleM7::GetSingleton()->Write(&ch, 1);
+        }
+        taskYIELD();
+    }
+}
---
+++ examples/freertos_task/freertos_task.cc
@@ -0,0 +1,43 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cstdio>
+
+#include "libs/base/led.h"
+#include "libs/base/tasks.h"
+#include "third_party/freertos_kernel/include/FreeRTOS.h"
+#include "third_party/freertos_kernel/include/task.h"
+
+// Starts a second task to blink one LED in each task.
+
+[[noreturn]] void blink_task(void* param) {
+    printf("Hello task.\r\n");
+    bool on = true;
+    while (true) {
+        on = !on;
+        coralmicro::led::Set(coralmicro::led::LED::kUser, on);
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
+extern "C" void app_main(void *param) {
+    xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, nullptr,
+                APP_TASK_PRIORITY, nullptr);
+    bool on = true;
+    while (true) {
+        on = !on;
+        coralmicro::led::Set(coralmicro::led::LED::kStatus, on);
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
