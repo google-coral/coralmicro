03e9adca18ed8eeeef9c3faef1383304e5622d80
Dmitry Kovalev <dkovalev@google.com>
2022-07-15 10:35:29 -0700

    Add copyrights to edgefast_bluetooth
    Change-Id: I08ccf8de44077b191cedb51700a98589e6d6802e
    GitOrigin-RevId: 08b12da30359a66210a8600e2c917a7dcf46b532

--- libs/nxp/rt1176-sdk/edgefast_bluetooth/edgefast_bluetooth.h
+++ libs/nxp/rt1176-sdk/edgefast_bluetooth/edgefast_bluetooth.h
@@ -1,5 +1,21 @@
-#ifndef _LIBS_NXP_RT1176_SDK_EDGEFAST_BLUETOOTH_EDGEFAST_BLUETOOTH_H_
-#define _LIBS_NXP_RT1176_SDK_EDGEFAST_BLUETOOTH_EDGEFAST_BLUETOOTH_H_
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef LIBS_NXP_RT1176_SDK_EDGEFAST_BLUETOOTH_EDGEFAST_BLUETOOTH_H_
+#define LIBS_NXP_RT1176_SDK_EDGEFAST_BLUETOOTH_EDGEFAST_BLUETOOTH_H_
 
 /* clang-format off */
 #include "third_party/nxp/rt1176-sdk/middleware/edgefast_bluetooth/include/toolchain.h"
@@ -9,4 +25,4 @@
 
 void InitEdgefastBluetooth(bt_ready_cb_t cb);
 
-#endif  // _LIBS_NXP_RT1176_SDK_EDGEFAST_BLUETOOTH_EDGEFAST_BLUETOOTH_H_
+#endif  // LIBS_NXP_RT1176_SDK_EDGEFAST_BLUETOOTH_EDGEFAST_BLUETOOTH_H_
--- libs/nxp/rt1176-sdk/edgefast_bluetooth/edgefast_bluetooth_config.h
+++ libs/nxp/rt1176-sdk/edgefast_bluetooth/edgefast_bluetooth_config.h
@@ -1,12 +1,21 @@
 /*
- * Copyright 2021 NXP
- * All rights reserved.
+ * Copyright 2022 Google LLC
  *
- * SPDX-License-Identifier: BSD-3-Clause
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-#ifndef __BLE_CONFIG_H__
-#define __BLE_CONFIG_H__
+#ifndef LIBS_NXP_RT1176_SDK_EDGEFAST_BLUETOOTH_EDGEFAST_BLUETOOTH_CONFIG_H_
+#define LIBS_NXP_RT1176_SDK_EDGEFAST_BLUETOOTH_EDGEFAST_BLUETOOTH_CONFIG_H_
 
 /*!
  * @brief Bluetooth Configuration
@@ -1736,4 +1745,4 @@ Select this for LE Peripheral role support.
 #endif /*(defined(CONFIG_BT_SPP) && (CONFIG_BT_SPP > 0))*/
 
 /*! @}*/
-#endif /* __BLE_CONFIG_H__ */
+#endif /* LIBS_NXP_RT1176_SDK_EDGEFAST_BLUETOOTH_EDGEFAST_BLUETOOTH_CONFIG_H_ */
