b235b7ee9faa08954c6063517dfa9ba8e8574380
Dmitry Kovalev <dkovalev@google.com>
2022-07-20 14:07:31 -0700

    Add filesystem docs
    Change-Id: Ib4c0aed5c179b2c49d391e312ebb4dac51b28b57
    GitOrigin-RevId: 191a869ce98557081462dbc107798c27201b2e1a

--- libs/base/filesystem.h
+++ libs/base/filesystem.h
@@ -25,19 +25,75 @@
 
 namespace coralmicro {
 
+// Returns littlefs instance to use with functions from `lfs.h`.
+//
+// @returns Pointer to littlefs instance.
 lfs_t* Lfs();
 
+// Initializes and mounts littlefs.
+//
+// @param force_format True to format filesystem on mount, false otherwise.
+// @returns True upon success, false otherwise.
 bool LfsInit(bool force_format = false);
 
+// Creates directory, similar to `mkdir -p <path>`.
+//
+// @param path Directory path.
+// @returns True upon success, false otherwise.
 bool LfsMakeDirs(const char* path);
+
+// Returns the string up to, but not including, the final '/' in the path.
+//
+// @param path Path.
+// @returns The string up to, but not including, the final '/' in the path.
 std::string LfsDirname(const char* path);
+
+// Returns file size.
+//
+// @param path File path.
+// @returns File size in bytes.
 ssize_t LfsSize(const char* path);
 
+// Returns whether path exists and represents a directory.
+//
+// @param path Path to check
+// @returns True if path exists and represents a directory, false otherwise.
 bool LfsDirExists(const char* path);
+
+// Returns whether path exists and represents a file.
+//
+// @param path Path to check
+// @returns true if path exists and represents a file, false otherwise.
 bool LfsFileExists(const char* path);
+
+// Reads content of the file and stores it in `std::vector<uint8_t>`.
+//
+// @param path File path to read.
+// @param buf Instance of `std::vector<uint8_t>` to read data in.
+// @returns True upon success, false otherwise.
 bool LfsReadFile(const char* path, std::vector<uint8_t>* buf);
+
+// Reads content of the file and stores it in `std::string`.
+//
+// @param path File path to read.
+// @param str Instance of `std::string` to read data in.
+// @returns True upon success, false otherwise.
 bool LfsReadFile(const char* path, std::string* str);
+
+// Reads content of the file and stores it in memory buffer.
+//
+// @param path File path to read.
+// @param buf Memory buffer to read data in.
+// @param size Memory buffer size.
+// @returns Number of read bytes not exceeding `size`.
 size_t LfsReadFile(const char* path, uint8_t* buf, size_t size);
+
+// Writes content of the memory buffer to file.
+//
+// @param path File path to write.
+// @param buf Memory buffer to write data from.
+// @param size Memory buffer size.
+// @returns True upon success, false otherwise.
 bool LfsWriteFile(const char* path, const uint8_t* buf, size_t size);
 
 }  // namespace coralmicro
