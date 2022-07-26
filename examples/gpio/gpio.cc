5043aa40ae857965fa777629194fa1e9425f1daf
Alex Van Damme <atv@google.com>
2022-07-26 10:28:13 -0700

    Add example for header GPIOs to help validate config
    - Quick example that toggles the GPIOs on the header up and down. Useful
      for validating that the pin configs are good.
    
    b/240203644
    
    Change-Id: If06f20d3c304476966fac31bbc5fe7debfb1fb19
    GitOrigin-RevId: ac2362f31148e219b9b337ea5cf4c0e79a1d037d

---
+++ examples/gpio/gpio.cc
@@ -0,0 +1,48 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "libs/base/gpio.h"
+
+#include "third_party/freertos_kernel/include/FreeRTOS.h"
+#include "third_party/freertos_kernel/include/task.h"
+
+namespace coralmicro {
+void Main() {
+  constexpr Gpio kGpiosToTest[] = {
+      kSpiCs, kSpiSck, kSpiSdo,  kSpiSdi,  kSda6, kScl1, kSda1,
+      kAA,    kAB,     kUartCts, kUartRts, kPwm1, kPwm0, kScl6,
+  };
+
+  for (auto gpio : kGpiosToTest) {
+    GpioSetMode(gpio, false, false, false);
+  }
+
+  printf("Periodically toggling header GPIOs...\r\n");
+
+  bool high = true;
+  while (true) {
+    for (auto gpio : kGpiosToTest) {
+      GpioSet(gpio, high);
+    }
+    high = !high;
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
+}  // namespace coralmicro
+
+extern "C" void app_main(void* param) {
+  (void)param;
+  coralmicro::Main();
+  vTaskSuspend(nullptr);
+}
