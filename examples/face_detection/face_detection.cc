498be441f8f1bcb7a4fe26de712b56003f4de03f
Nam Vu <vunam@google.com>
2022-08-03 09:41:46 -0500

    Added face detection example.
    Fixes b/241185478.
    
    Change-Id: I4ee497d66997f37e4c7a03bf7b9ec7f44daf87e3
    GitOrigin-RevId: 5827982dd5865f7883c8efd5088f014815bd4033

---
+++ examples/face_detection/face_detection.cc
@@ -0,0 +1,118 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cstring>
+#include <vector>
+
+#include "libs/base/filesystem.h"
+#include "libs/base/led.h"
+#include "libs/camera/camera.h"
+#include "libs/tensorflow/detection.h"
+#include "libs/tensorflow/utils.h"
+#include "libs/tpu/edgetpu_manager.h"
+#include "libs/tpu/edgetpu_op.h"
+#include "third_party/freertos_kernel/include/FreeRTOS.h"
+#include "third_party/freertos_kernel/include/task.h"
+#include "third_party/tflite-micro/tensorflow/lite/micro/micro_error_reporter.h"
+#include "third_party/tflite-micro/tensorflow/lite/micro/micro_interpreter.h"
+#include "third_party/tflite-micro/tensorflow/lite/micro/micro_mutable_op_resolver.h"
+
+namespace coralmicro {
+namespace {
+constexpr char kModelPath[] =
+    "/models/ssd_mobilenet_v2_face_quant_postprocess_edgetpu.tflite";
+constexpr int kTopK = 5;
+constexpr float kThreshold = 0.5;
+
+// An area of memory to use for input, output, and intermediate arrays.
+constexpr int kTensorArenaSize = 8 * 1024 * 1024;
+STATIC_TENSOR_ARENA_IN_SDRAM(tensor_arena, kTensorArenaSize);
+
+[[noreturn]] void Main() {
+  std::vector<uint8_t> model;
+  if (!LfsReadFile(kModelPath, &model)) {
+    printf("ERROR: Failed to load %s\r\n", kModelPath);
+    vTaskSuspend(nullptr);
+  }
+
+  auto tpu_context = EdgeTpuManager::GetSingleton()->OpenDevice();
+  if (!tpu_context) {
+    printf("ERROR: Failed to get EdgeTpu context\r\n");
+    vTaskSuspend(nullptr);
+  }
+
+  tflite::MicroErrorReporter error_reporter;
+  tflite::MicroMutableOpResolver<3> resolver;
+  resolver.AddDequantize();
+  resolver.AddDetectionPostprocess();
+  resolver.AddCustom(kCustomOp, RegisterCustomOp());
+
+  tflite::MicroInterpreter interpreter(tflite::GetModel(model.data()), resolver,
+                                       tensor_arena, kTensorArenaSize,
+                                       &error_reporter);
+  if (interpreter.AllocateTensors() != kTfLiteOk) {
+    printf("ERROR: AllocateTensors() failed\r\n");
+    vTaskSuspend(nullptr);
+  }
+
+  if (interpreter.inputs().size() != 1) {
+    printf("ERROR: Model must have only one input tensor\r\n");
+    vTaskSuspend(nullptr);
+  }
+
+  // Starting Camera.
+  CameraTask::GetSingleton()->SetPower(true);
+  CameraTask::GetSingleton()->Enable(CameraMode::kStreaming);
+
+  auto* input_tensor = interpreter.input_tensor(0);
+  int model_height = input_tensor->dims->data[1];
+  int model_width = input_tensor->dims->data[2];
+
+  while (true) {
+    CameraFrameFormat fmt{CameraFormat::kRgb,
+                          CameraFilterMethod::kBilinear,
+                          CameraRotation::k0,
+                          model_width,
+                          model_height,
+                          false,
+                          tflite::GetTensorData<uint8_t>(input_tensor)};
+    if (!CameraTask::GetSingleton()->GetFrame({fmt})) {
+      printf("Failed to capture image\r\n");
+      vTaskSuspend(nullptr);
+    }
+
+    if (interpreter.Invoke() != kTfLiteOk) {
+      printf("Failed to invoke\r\n");
+      vTaskSuspend(nullptr);
+    }
+
+    if (auto results =
+            tensorflow::GetDetectionResults(&interpreter, kThreshold, kTopK);
+        !results.empty()) {
+      printf("Found %d face(s):\r\n%s\r\n", results.size(),
+             tensorflow::FormatDetectionOutput(results).c_str());
+      LedSet(Led::kUser, true);
+    } else {
+      LedSet(Led::kUser, false);
+    }
+  }
+}
+
+}  // namespace
+}  // namespace coralmicro
+
+extern "C" void app_main(void* param) {
+  (void)param;
+  coralmicro::Main();
+}
