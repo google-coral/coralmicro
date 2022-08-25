39ba74287fc5d6004b2f5620d42d10c9cfe4e7c8
Jason Israel <jasonisrael@google.com>
2022-08-25 12:56:03 -0700

    :Add Camera Streaming Http example
    Change-Id: I85d4f9abc47681a486d54b8fccd0f29cd0f95ab4
    GitOrigin-RevId: a0a6f870ca0ebe443c44ed8006683da9a2c8c64a

---
+++ sketches/CameraStreamingHttp/CameraStreamingHttp.ino
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <coralmicro_camera.h>
+
+#include "Arduino.h"
+#include "libs/base/http_server.h"
+#include "libs/base/strings.h"
+#include "libs/base/utils.h"
+#include "libs/libjpeg/jpeg.h"
+
+using namespace coralmicro::arduino;
+
+namespace {
+coralmicro::HttpServer http_server;
+FrameBuffer frame_buffer;
+
+const int width = 324;
+const int height = 324;
+
+constexpr char kIndexFileName[] = "/coral_micro_camera.html";
+constexpr char kCameraStreamUrlPrefix[] = "/camera_stream";
+}  // namespace
+
+coralmicro::HttpServer::Content UriHandler(const char* uri) {
+  Serial.print("Request received for ");
+  Serial.println(uri);
+  if (coralmicro::StrEndsWith(uri, "index.shtml")) {
+    return std::string(kIndexFileName);
+  } else if (coralmicro::StrEndsWith(uri, kCameraStreamUrlPrefix)) {
+    if (!Camera.grab(frame_buffer) == CameraStatus::SUCCESS) {
+      return {};
+    }
+
+    std::vector<uint8_t> jpeg;
+    coralmicro::JpegCompressRgb(frame_buffer.getBuffer(), width, height,
+                                /*quality=*/75, &jpeg);
+    return jpeg;
+  }
+  return {};
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Arduino Camera HTTP Example!");
+
+  // Turn on Status LED to show the board is on.
+  pinMode(PIN_LED_STATUS, OUTPUT);
+  digitalWrite(PIN_LED_STATUS, HIGH);
+
+  // Start Camera
+  if (Camera.begin(width, height) != CameraStatus::SUCCESS) {
+    Serial.println("Failed to start camera");
+    return;
+  }
+
+  Serial.println("Starting server...");
+  http_server.AddUriHandler(UriHandler);
+  coralmicro::UseHttpServer(&http_server);
+
+  std::string ip;
+  if (coralmicro::GetUsbIpAddress(&ip)) {
+    Serial.print("GO TO:   http://");
+    Serial.print(ip.c_str());
+  }
+  http_server.AddUriHandler(UriHandler);
+  UseHttpServer(&http_server);
+}
+
+void loop() {}
